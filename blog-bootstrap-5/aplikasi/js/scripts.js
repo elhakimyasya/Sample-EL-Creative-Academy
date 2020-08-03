2bad077c5b1a9c0bdf2374cba7f8488db2b0b4fa
Yasya El Hakim <yasyaelhakim@gmail.com>
2020-08-04 02:01:05 +0700

    Update

--- blog-bootstrap-5/aplikasi/js/scripts.js
+++ blog-bootstrap-5/aplikasi/js/scripts.js
@@ -2,3 +2,11 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 	return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+var daftarAkun = document.getElementById('daftarAkun');
+var modalDaftarAkun = new bootstrap.Modal(document.getElementById('modalDaftarAkun'), {
+	keyboard: false
+})
+daftarAkun.addEventListener('click', function () {
+	modalDaftarAkun.toggle();
+})
